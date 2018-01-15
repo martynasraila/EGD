80845c17e9b5a622e19cc600313b9e2773d5bb3b
Deividas Bakanas <deividas@quatrodev.com>
2018-01-15 02:02:39 +0200

    User title implemented.

--- Web.Frontend/src/app/modules/layout/header/layout-header.tsx
+++ Web.Frontend/src/app/modules/layout/header/layout-header.tsx
@@ -1,16 +1,16 @@
 import * as React from "react";
-import { UserBox } from "./user-box/user-box";
 
 import { HomeButton } from "./home-button/home-button";
 
 import "./layout-header.css";
+import { UserBoxContainer } from "./user-box/container/user-box-container";
 
 export class LayoutHeader extends React.Component {
     public render(): JSX.Element {
         return <div className="layout-header">
             <HomeButton />
             <div className="layout-navigation">layout navigation</div>
-            <UserBox userTitle="Vardenis Pavardenis" />
+            <UserBoxContainer />
         </div>;
     }
 }
